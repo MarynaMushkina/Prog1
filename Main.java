b0775f3965841dd7e1b8ccb88aa13801464f6d85
MarynaMushkina <mushkina.m@gmail.com>
2020-04-20 19:08:57 +0300

    update print

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.mary;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test Github");
+        System.out.println("Test Github 111");
     }
 }
